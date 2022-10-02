840d8d423cb2c1d9aa35106ec0f61fb9a16832b3
Andres <andrsbtrg@gmail.com>
2022-10-02 19:59:51 +0200

    player done

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,6 +4,10 @@ import { useEffect, useRef } from "react";
 import { Vector3 } from "three";
 import { useKeyboard } from "../hooks/useKeyboard";
 
+const JUMP_FORCE = 3;
+Object.freeze(JUMP_FORCE);
+const SPEED = 4;
+
 export const Player = () => {
     const actions = useKeyboard();
     console.log('actions', Object.entries(actions).filter(([k,v])=> v));
@@ -29,9 +33,34 @@ export const Player = () => {
     }, [api.position])
 
     useFrame(() => {
-        // console.log(pos);
+        const direction = new Vector3();
+
+        const frontVector = new Vector3(
+            0,
+            0,
+            (actions.moveBackward ? 1 : 0) - (actions.moveForward ? 1 : 0)
+        );
+
+        const sideVector = new Vector3(
+            (actions.moveLeft? 1: 0) - (actions.moveRight ? 1 : 0),
+            0,
+            0
+        );
+
+        direction
+            .subVectors(frontVector, sideVector)
+            .normalize()
+            .multiplyScalar(SPEED)
+            .applyEuler(camera.rotation);
+
+        api.velocity.set(direction.x, vel.current[1], direction.z);
+
         camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[2]));
-        api.velocity.set(0, 0, 0);
+        
+        if(actions.jump && Math.abs(vel.current[1] < 0.05)) {
+            api.velocity.set(vel.current[0], JUMP_FORCE, vel.current[2]);
+        }
+
     })
     return (
         <mesh ref={ref}>
